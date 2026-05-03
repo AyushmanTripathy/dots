d8685e21ab8ee6435192337d3c4b738ced04b765
Suprada-2002 <supradatripathy789@gmail.com>
2026-05-03 20:21:27 +0530

    backup Sun May  3 08:21:27 PM IST 2026

--- backups/nvim/lua/ayush/mappings.lua
+++ backups/nvim/lua/ayush/mappings.lua
@@ -32,7 +32,7 @@ normalmap("<Tab>o", ":FzfLua builtin <cr>")
 normalmap("<Tab>w", ":FzfLua buffers <cr>")
 normalmap("<Tab>g", ":FzfLua live_grep " .. fullscreen_vertical(60) .. "<cr>")
 
-normalmap("<Tab>j", ":FzfLua live_grep <cr>")
+normalmap("<Tab>j", ":FzfLua resume <cr>")
 
 normalmap("<leader>g", ":FzfLua git_commits " .. fullscreen_vertical(85) .. "<cr>")
 normalmap("<leader>c", ":FzfLua git_bcommits " .. fullscreen_vertical(85) .. "<cr>")
